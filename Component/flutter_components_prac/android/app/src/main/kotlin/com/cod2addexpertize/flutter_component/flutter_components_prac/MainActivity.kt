97a23d7865c916f63a896d8e667e01292d18d185
@Code2AddExpertize <code2addexpertize@gmail.com>
2023-10-18 12:40:27 +0530

    All Files Deleted

--- Component/flutter_components_prac/android/app/src/main/kotlin/com/cod2addexpertize/flutter_component/flutter_components_prac/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.cod2addexpertize.flutter_component.flutter_components_prac
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Component/flutter_gradient_prac/android/app/src/main/kotlin/com/code2addexpertize/gradient/flutter_gradient_prac/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.code2addexpertize.gradient.flutter_gradient_prac
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
